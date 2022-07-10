71160b5b7a1b13b34112330e9c4bee3913d790a3
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-07-10 21:45:51 +0900

    scraped books title with two star ratings

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import bs4
 import requests
 
-page_num = 12
 base_URL = "http://books.toscrape.com/catalogue/page-{}.html"
-res = requests.get(base_URL.format(1))
 
-print(base_URL)
+req = requests.get(base_URL.format(1))
+soup = bs4.BeautifulSoup(req.text, 'html.parser')
+product = (soup.select(".product_pod"))
+
+title = product[0]
+print(title.select("a")[1]['title']) # i can get title now
+
+two_stars_title = []
+
+for n in range(1,51):
+    scrape_url = base_URL.format(n)
+    req = requests.get(scrape_url)
+
+    soup = bs4.BeautifulSoup(req.text,'html.parser')
+    books = soup.select(".product_pod")
+
+    for book in books:
+        if len(book.select('star-rating.Two')) != 0:
+            book_title = book.select('a')[1]['title']
+            two_stars_title.append(book_title)
